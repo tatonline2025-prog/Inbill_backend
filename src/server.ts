3465e024f8f251a10e057ca00c42e0099019ae34
Vo Dang <vodang.work@gmail.com>
2025-10-08 16:05:01 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ const allowedOrigins = [
 const app = express();
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
